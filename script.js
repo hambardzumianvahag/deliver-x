534280ad087e790d43cdb4654cb4ef4cecd75f23
hambardzumian_v <hambardzumianvahag@gmail.com>
2024-08-05 20:02:59 +0400

    created new web application 'Deliver X'

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const burger = document.querySelector(".header__burger");
+  const menu = document.querySelector(".header__menu");
+
+  burger.addEventListener("click", () => {
+    menu.classList.toggle("show");
+  });
+});
